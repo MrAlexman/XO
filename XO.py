41ff7b49a6fe25ef2c73328313f2e5544a8f5aff
MrAlexman <lastik-fatcat@yandex.ru>
2022-11-09 12:59:17 +0300

    1. Included exit condition in main cycle 2. Configurated output in function "your_turn" 3. Made new function "winner", that returns True if "XO" has win-combination of 'X' or 'O'

--- XO.py
+++ XO.py
@@ -1,21 +1,54 @@
-def your_turn (cell_num):
+def your_turn(cell_num):
     file = open("XO.txt", "r")
     maket = ''.join(file.readlines())
-    return maket.format(cell_num[0], cell_num[1], cell_num[2], cell_num[3],\
-                        cell_num[4], cell_num[5], cell_num[6], cell_num[7], cell_num[8])
+    # return maket.format(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+    return maket.format(cell_num[0], cell_num[1], cell_num[2], cell_num[3], cell_num[4],
+                        cell_num[5], cell_num[6], cell_num[7], cell_num[8], cell_num[9])
+
+
+# print(your_turn([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]))
+
+def winner(cell_num, who):
+    poser = []
+    win = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9]]
+    for k, v in enumerate(cell_num):
+        if v == who:
+            poser.append(k)
+    for i in win:
+        # print(i)
+        counter = 0
+        for j in i:
+            if j in poser:
+                counter += 1
+        if counter == 3:
+            return True
+    return False
+
 
 num_turn = 0
-cell = [0, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
-while num_turn <= 8: #Здесь изменения
+cell = [0, '_', '_', '_', '_', '_', '_', '_', '_', '_']
+turns = set()
+print(your_turn(cell))
+while num_turn <= 8:
     turn = None
     if num_turn % 2 == 0:
         who = 'X'
     else:
         who = 'O'
     while turn not in [1, 2, 3, 4, 5, 6, 7, 8, 9]:
-        turn = int(input(f"Введите номер ячейки. Ход {who}: "))
+        print("Ввод числа от 1 до 9.")
+        try:
+            turn = int(input(f"Введите номер ячейки. Ход {who}: "))
+        except:
+            turn = None
+        if turn not in turns:
+            turns.add(turn)
+        else:
+            print("Не жульничай!")
+            turn = None
     cell[turn] = who
-    num_turn+=1
-
-
-
+    print(your_turn(cell))
+    if winner(cell, who):
+        print(f"Побеза за {who}!")
+        break
+    num_turn += 1
